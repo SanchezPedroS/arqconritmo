87061433b457ef74fddd5ae2221bf04062111b8e
SanchezPedroS <106620491+SanchezPedroS@users.noreply.github.com>
2022-06-03 16:39:30 -0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+ 
+ $name = $_POST['name' + 'lname'];
+ $email = $_POST['email'];
+ $phone = $_POST['phone'];
+ $message = $_POST['message'];
+ $para = 'pedro.ssanchez@email.com';
+ $titulo = 'Hola, queria mas informacion';
+  
+ $msjCorreo = "Nombre: $name\n E-Mail: $email\n Telefono: $phone\n Mensaje:\n $message";
+  
+ if ($_POST['submit']) {
+ if (mail ($para, $titulo, $msjCorreo)) {
+ echo 'Gracias por comunicarse con nosotros, a la brevedad nos pondremos en contacto';
+ } else {
+ echo 'Falló el envio, intente nuevamente';
+ }
+ }
+ 
+?>
